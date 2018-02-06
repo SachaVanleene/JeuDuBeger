7ce893551261d341f8e64194c92b893c80f2a2de
Unknown <debay.theo@gmail.com>
2018-02-06 18:30:11 -0500

    Ajout terrain

---
+++ Assets/sheep/walk.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0, 0.1f);
+	}
+}
