20ba8ee0027a1b74f3bf840fa8476e0c30680725
WillouPouch <wilfried.pouchous@gmail.com>
2018-02-06 18:41:43 -0500

    Prefabs mouton + 2 enclos

---
+++ Assets/sheep/walk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour {
+
+    //Variables
+    private Vector3 direction = new Vector3(0, 0, 0.002f);
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (direction.x, direction.y, direction.z);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Fences") || other.gameObject.CompareTag("Sheep")) {
+            transform.Rotate(0, 180, 0);
+            transform.Translate(direction.x+0.005f, direction.y, direction.z); //poussé pour éviter problème quand collé à la barrière
+        }
+    }
+}
