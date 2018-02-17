3a0d5034500f41a0d14dd59ae8a147648f0da0ac
Hugo Brunet <hugo.brunet@isen-lille.fr>
2018-02-16 22:06:04 -0500

    Add managers.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Script.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        public static GameManager instance = null;
+
+
+        private int _roundNumber;                 // Which round the game is currently on.
+        private WaitForSeconds _startWait;         // Used to have a delay whilst the round starts.
+        private WaitForSeconds _endWait;           // Used to have a delay whilst the round or game ends.
+        void Awake()
+        {
+            //Check if instance already exists
+            if (instance == null)
+
+                //if not, set instance to this
+                instance = this;
+
+            //If instance already exists and it's not this:
+            else if (instance != this)
+
+                //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+                Destroy(gameObject);
+
+            //Sets this to not be destroyed when reloading scene
+            DontDestroyOnLoad(gameObject);
+
+            //Call the InitGame function to initialize the first level 
+            //InitGame();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
