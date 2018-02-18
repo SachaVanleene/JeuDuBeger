a7588e0a6ec7a371c02becb26c69cb42804dd88f
Hugo Brunet <hugo.brunet@isen.yncrea.fr>
2018-02-18 18:35:39 -0500

    Separate night start & day end

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Script.Managers
 {
@@ -8,12 +9,14 @@ namespace Assets.Script.Managers
     {
 
         public static GameManager instance = null;
+        public float dayCycleDelay = 30f;
+        public float nightCycleEndingDelay = 5f;
+        public Text messageText;
 
+        private int _roundNumber = 0;                 // Which round the game is currently on.
+        private WaitForSeconds _nightCycleEndingWait;           // Used to have a delay whilst the round or game ends.
 
-        private int _roundNumber;                 // Which round the game is currently on.
-        private WaitForSeconds _startWait;         // Used to have a delay whilst the round starts.
-        private WaitForSeconds _endWait;           // Used to have a delay whilst the round or game ends.
-        void Awake()
+        private void Awake()
         {
             //Check if instance already exists
             if (instance == null)
@@ -30,14 +33,81 @@ namespace Assets.Script.Managers
             //Sets this to not be destroyed when reloading scene
             DontDestroyOnLoad(gameObject);
 
-            //Call the InitGame function to initialize the first level 
-            //InitGame();
         }
 
-        // Update is called once per frame
+        private void Start()
+        {
+            StartCoroutine(GameLoop());
+        }
+
         void Update()
         {
 
+        }
+
+        private IEnumerator GameLoop()
+        {
+            yield return StartCoroutine(DayCycleStarting());
+
+            yield return StartCoroutine(DayCyclePlaying());
+
+            yield return StartCoroutine(DayCycleEnding());
+
+            yield return StartCoroutine(NightCycleStarting());
+
+            yield return StartCoroutine(NightCyclePlaying());
+
+            yield return StartCoroutine(NightCycleEnding());
+
+            StartCoroutine(GameLoop());
+        }
+
+        private IEnumerator DayCycleStarting()
+        {
+            //TODO: Enable traps placement, sheeps interactions, player control. Start day light.
+
+            _roundNumber++;
+            messageText.text = "ROUND " + _roundNumber;
+
+            yield return null;
+        }
+
+        private IEnumerator DayCyclePlaying()
+        {
+            messageText.text = string.Empty;
+            float localDayCycleDelay = Time.time + dayCycleDelay;
+            
+            while (!Input.GetKeyDown(KeyCode.K) ||  Time.time  < localDayCycleDelay)
+            {
+                yield return null;
+            }
+        }
+
+        private IEnumerator DayCycleEnding()
+        {
+            //TODO: Disable traps placement, sheeps interactions. Start night light.
+
+            yield return null;
+        }
+
+        private IEnumerator NightCycleStarting()
+        {
+            yield return null;
+        }
+        private IEnumerator NightCyclePlaying()
+        {
+            //TODO: while there are wolves alive or there is one sheep
+
+            yield return null;
+        }
+
+        private IEnumerator NightCycleEnding()
+        {
+            //TODO: Disable player control
+
+            //TODO: Round recap or game recap if game over.
+
+            yield return null;
         }
     }
 }
