cc56f411d65f4285e1fb14e41c29b8bbb57728d8
Unknown <debay.theo@gmail.com>
2018-02-21 14:48:49 -0500

    Ajout d'arbre + asset roche

---
+++ Assets/Scripts/Wolf/WolfBossHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfBossHealth : MonoBehaviour
+{
+
+    private Animator anim;
+    private ParticleSystem cloud;
+    private int health;
+
+    private void Awake()
+    {
+        health = 100;
+        anim = GetComponent<Animator>();
+        cloud = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        //anim.SetTrigger("Hit");
+
+        if (health < 0)
+        {
+            GetComponent<IA_Wolves_Path>().updateTarget(null);
+            anim.SetTrigger("dead");
+            Destroy(gameObject, 2.5f);
+        }
+
+    }
+
+    public void setHealth(int newHealth)
+    {
+        health = newHealth;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+}
+
