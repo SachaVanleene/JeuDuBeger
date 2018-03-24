b573685128f8f88c4a2f0a2b1c2756f5023bc19c
ArnaudMONTEILS <arnaud.monteils@isen-lille.fr>
2018-03-24 17:40:03 -0400

    adding trapSelector

---
+++ Assets/Scripts/Traps/SelectionElement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class SelectionElement : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start ()
+        {
+            transform.localScale = Vector3.one * 0.8f;
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+
+        public void Select()
+        {
+           // transform.GetChild(0).gameObject.SetActive(true);
+            transform.localScale = Vector3.one;
+        }
+        public void Unselect()
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.localScale = Vector3.one * 0.8f;
+        }
+    }
+}
