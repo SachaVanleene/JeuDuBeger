ac9817d48bc0cfc37da036d036617d6bbbdd16bc
Sacha vanleen <sacha1231@hotmail.fr>
2018-04-11 20:45:41 -0400

    Revert " conflict pour accéder à une fonction"
    This reverts commit e0cba2d2288c7775b359203ac3927d7aac6daca2.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -352,7 +352,7 @@ namespace Assets.Script.Managers
                 cycleManager.NextCycle(GameVariables.Cycle.passedCycleSpeed);
             }
         }
-        public void callAchievement(AchievementEvent achEvent, int step = 1)
+        private void callAchievement(AchievementEvent achEvent, int step = 1)
         {
             List<AchievementInfo> endedAchievements = SProfilePlayer.getInstance().AchievementsManager.AddStepAchievement(achEvent, step);
             // hack fix to prevent bug when quitting the game
